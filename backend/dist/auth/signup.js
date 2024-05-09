2bb5b4eba8450462202f490ec5e2315d9b9562d7
Rohan G <2001.rohangupta@gmail.com>
2024-05-09 22:46:29 +0530

    signup completion, bcrypt added

--- backend/dist/auth/signup.js
+++ backend/dist/auth/signup.js
@@ -8,9 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const prisma = new client_1.PrismaClient();
+function isValidEmail(email) {
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 function SignupController(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -18,6 +27,13 @@ function SignupController(req, res) {
             if (!user.username || !user.email || !user.password) {
                 return res.json({
                     message: "Please provide all the credentials!",
+                    success: false,
+                    path: null
+                });
+            }
+            if (!isValidEmail(user.email)) {
+                return res.status(400).json({
+                    message: "Invalid email format",
                     success: false,
                     path: null
                 });
@@ -44,17 +60,19 @@ function SignupController(req, res) {
                     });
                 }
             }
+            const hashedPassword = (yield bcrypt_1.default.hash(user.password, 10)).toString();
+            // Creates a new user
             const newUser = yield prisma.user.create({
                 data: {
                     username: user.username,
                     email: user.email,
-                    password: user.password
+                    password: hashedPassword
                 }
             });
             return res.status(200).json({
                 message: "User created successfully",
                 success: true,
-                path: "/home"
+                path: "/login"
             });
         }
         catch (error) {
--- backend/src/auth/signup.ts
+++ backend/src/auth/signup.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -9,11 +10,17 @@ interface User {
     password: string
 }
 
+function isValidEmail(email: string): boolean {
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 export default async function SignupController(req: Request, res: Response) {
     try {
         const user: User = req.body;
 
-        if(!user.username || !user.email || !user.password) {
+        if (!user.username || !user.email || !user.password) {
             return res.json({
                 message: "Please provide all the credentials!",
                 success: false,
@@ -21,7 +28,15 @@ export default async function SignupController(req: Request, res: Response) {
             })
         }
 
-        if(prisma.user) {
+        if (!isValidEmail(user.email)) {
+            return res.status(400).json({
+                message: "Invalid email format",
+                success: false,
+                path: null
+            })
+        }
+
+        if (prisma.user) {
             // Checks for an existing user
             const existingUser: User | null = await prisma.user.findUnique({
                 where: {
@@ -29,7 +44,7 @@ export default async function SignupController(req: Request, res: Response) {
                 }
             });
 
-            if((existingUser?.email === user.email) && (existingUser.username !== user.username)) {
+            if ((existingUser?.email === user.email) && (existingUser.username !== user.username)) {
                 return res.status(409).json({
                     message: "Email is already in use! Please try another one.",
                     success: false,
@@ -37,7 +52,7 @@ export default async function SignupController(req: Request, res: Response) {
                 })
             }
 
-            if((existingUser?.email === user.email) && (existingUser.username === user.username)) {
+            if ((existingUser?.email === user.email) && (existingUser.username === user.username)) {
                 return res.status(409).json({
                     message: "You already have an account! Please login.",
                     success: false,
@@ -46,18 +61,21 @@ export default async function SignupController(req: Request, res: Response) {
             }
         }
 
+        const hashedPassword: string = (await bcrypt.hash(user.password, 10)).toString();
+
+        // Creates a new user
         const newUser = await prisma.user.create({
             data: {
                 username: user.username,
                 email: user.email,
-                password: user.password
+                password: hashedPassword
             }
         })
 
         return res.status(200).json({
             message: "User created successfully",
             success: true,
-            path: "/home"
+            path: "/login"
         })
     } catch (error) {
         console.log(error);

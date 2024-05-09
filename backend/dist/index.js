18f5ee91105bc879cf36225951d857977a9c4af4
Rohan G <2001.rohangupta@gmail.com>
2024-05-09 18:51:07 +0530

    ts

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,6 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send("Hello world");
+    console.log("Hello");
+});
 app.listen(8000, () => {
     console.log("Server on port 8000 fcberhyjkvf");
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world");
+    console.log("Hello");
+})
 
 app.listen(8000, () => {
     console.log("Server on port 8000 fcberhyjkvf");

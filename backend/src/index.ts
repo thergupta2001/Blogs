e9515770240655e7749b2d6f03a4d480fb9c559f
Rohan G <2001.rohangupta@gmail.com>
2024-05-21 11:17:45 +0530

    test 7

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-    origin: "https://blogs-two-beryl.vercel.app/",
+    origin: ["https://blogs-two-beryl.vercel.app"],
     credentials: true
 }));
 

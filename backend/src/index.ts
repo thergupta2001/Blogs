9eea527d3fd3bcd392468476f102ef9ada9bb088
thergupta2001 <2001.rohangupta@gmail.com>
2024-05-31 23:14:13 +0530

    cors headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,6 @@ import cron from "node-cron";
 import { deleteExpiredOTPs } from "./auth/deleteOtp";
 import userRouter from "./user/router";
 import jwt, { JwtPayload, decode } from "jsonwebtoken";
-// import compression from "compression";
 
 dotenv.config();
 
@@ -43,15 +42,14 @@ const limiter = rateLimit({
 // app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://blogs-one-tawny.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-});
-// app.use(compression());
+})
 
 app.use(cookieParser());
 
